f6766382629eebf53c209daa43470b311634180c
ttiverson3 <ttiverson3@yahoo.com.tw>
2018-11-22 02:52:17 +0800

    Update Fibonacci.java

--- recursive/Fibonacci.java
+++ recursive/Fibonacci.java
@@ -4,6 +4,7 @@ public class Fibonacci {
 		int n =10;
 		System.out.println("F" + n + "=" + fib(n));
 		System.out.println("F" + n + "=" + fib_rec(n));
+		System.out.println("F" + n + "=" + fibDP(n));
 	}
 	public static int fib(int n) {
 		//forloop
@@ -23,5 +24,19 @@ public class Fibonacci {
 			return fib_rec(n-1)+fib_rec(n-2);
 		}
 	}
+	public static int fibDP(int n) {
+		//Dynamic Programming
+		int a[]=new int[n+1];
+		a[0]=0;
+		a[1]=1;
+		if(n==0) return a[0];
+		else if(n==1) return a[1];
+		else {
+		for(int i=2;i<=n;i++) {
+			a[i]=a[i-1]+a[i-2];
+		}
+	}
+		return a[n];
+}
 
 }
