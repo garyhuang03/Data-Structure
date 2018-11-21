195fbb15433d8b95f67e72aec47d3e4752becb2e
ttiverson3 <ttiverson3@yahoo.com.tw>
2018-11-22 04:05:46 +0800

    Update Ackerman.java

--- recursive/Ackerman.java
+++ recursive/Ackerman.java
@@ -3,16 +3,11 @@ public class Ackerman {
 	public static void main(String[]arg) {
 		int m=2,n=2;
 		System.out.println("Ackerman("+m+","+n+")="+ack(m,n));
-		
 	}
 	public static int ack(int m,int n) {
-		
-		if(m==0)
-			return n+1;
-		else if(n==0)
-			return ack(m-1,1);
-		else
-			return ack(m-1,ack(m,n-1));
+		if(m==0) return n+1;
+		else if(n==0)return ack(m-1,1);
+		else return ack(m-1,ack(m,n-1));
 	}
 
 }
