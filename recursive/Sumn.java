1cf36401e1036ea98979341c338e2e495171b399
ttiverson3 <ttiverson3@yahoo.com.tw>
2018-11-22 02:21:49 +0800

    Create Sumn.java

---
+++ recursive/Sumn.java
@@ -0,0 +1,30 @@
+
+public class Sumn {
+	public static void main(String[]arg) {
+		int n=10;
+		System.out.println("sum 1 to " + n + "=" + sum(n)); //forloop
+		System.out.println("sum 1 to " + n + "=" + sum_rec(n)); //recursive
+		
+	}
+	public static int sum(int n) 
+	{
+		//°j°é 
+		int sum=0,i;
+		for(i=1;i<=n;i++) 
+		{
+			sum=sum+i;
+		}
+			return sum;
+	}
+	
+	public static int sum_rec(int n)  
+	{
+		//»¼°j
+		if(n==0) return 0;
+		else 
+		{
+			return n + sum_rec(n-1);
+		}
+	}
+
+}
