bbf7f9f2c8ea96791c934d32a740b63e419db44b
Gary Huang <45207364+ttiverson3@users.noreply.github.com>
2018-11-23 22:21:50 +0800

    Update Logn.java

--- recursive/Logn.java
+++ recursive/Logn.java
@@ -2,9 +2,10 @@
 public class Logn {
 	public static void main(String[] arg) {
 		int x=16,n=2;
-		System.out.println("Log¥H"+n+"¬°°ò©³"+"¨ú"+x+"="+log(x,n));
+		System.out.println("Logä»¥"+n+"ç‚ºåŸºåº•å–"+x+"="+log(x,n));
 	}
 	public static int log(int x,int n) {
+		
 		if(n<=1) System.out.println("error");
 		if(x<n) return 0;
 		x=x/n;
