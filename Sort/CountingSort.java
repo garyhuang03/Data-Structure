734778818ec7cd9a1622da2e59683b2847ddcb60
Gary Huang <45207364+ttiverson3@users.noreply.github.com>
2018-11-24 00:34:11 +0800

    Update CountingSort.java

--- Sort/CountingSort.java
+++ Sort/CountingSort.java
@@ -2,7 +2,7 @@
 public class CountingSort {
 	public static void main(String[] arg) {
 		int a[]= {1,2,0,1,5,0,2,2,1,5};
-		int k=5;//
+		int k=5;//鍵值範圍0~5
 		int n=a.length;
 		System.out.println("Before CountingSort:");
 		print(a);
@@ -14,24 +14,24 @@ public class CountingSort {
 	public static void count(int a[],int n,int k) {
 		int output[]=new int[n];
 		int count[]=new int[k+1];
-		//init
+		//init初始化
 		for(int i=0;i<=k;i++) {
 			count[i]=0;
 		}
-		//count
+		//count鍵值出現次數
 		for(int i=0;i<n;i++) {
 			count[a[i]]++;
 		}
-		//
+		//計算各鍵值結束位置
 		for(int i=1;i<=k;i++) {
 			count[i]+=count[i-1];
 		}
-		//output
+		//從各鍵值的最後位置開始放
 		for(int i=n-1;i>=0;i--) {
 			output[count[a[i]]-1]=a[i];
 			count[a[i]]--;
 		}
-		//
+		//將output存回a
 		for(int i=0;i<n;i++) {
 			a[i]=output[i];
 		}
