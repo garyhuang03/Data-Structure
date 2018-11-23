9287c01ab04e4654476894569d6b60f124abc17a
ttiverson3 <ttiverson3@yahoo.com.tw>
2018-11-24 02:41:36 +0800

    Create BinarySearch.c

---
+++ BinarySearch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int BS(int *a,int k,int l,int u)
+{
+    while(l<u){
+        int mid=(l+u)/2;
+        if(k<a[mid])
+        {
+            u=mid-1;
+        }
+        else if(k>a[mid])
+        {
+            l=mid+1;
+        }
+        else return mid;
+    }
+    return -1;
+}
+
+int BS_rec(int *a,int k,int l,int u)
+{
+    if(l<u){
+        int mid=(l+u)/2;
+        if(k<a[mid])
+          return BS_rec(a,k,l,mid-1);
+        else if(k>a[mid])
+          return BS_rec(a,k,mid+1,u);
+        else return mid;  
+    }
+    return -1;
+}
+
+
+int main()
+{
+    int a[]= {22,55,5,4,88,9,75,3};
+	int n=sizeof(a)/sizeof(a[0]);
+	int l=0,u=n-1;
+	int k=9;
+    BS(a,k,l,u);
+    printf("%d\n",BS(a,k,l,u));
+    printf("%d",BS_rec(a,k,l,u));
+    return 0;
+}
