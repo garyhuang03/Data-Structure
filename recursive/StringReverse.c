e53c9bf528b44e22231dbae99e6047859614f973
Gary Huang <45207364+ttiverson3@users.noreply.github.com>
2018-11-28 01:17:12 +0800

    Update StringReverse.c

--- recursive/StringReverse.c
+++ recursive/StringReverse.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-//105中山大學資料結構
-void reverse(char *s)
+//105,107中山大學
+int reverse(int n)
+{
+    if(n>0)
+    {
+        int a=n%10;
+        printf("%d",a);
+        return reverse(n/10);
+    }
+}
+
+int reverse_char(char*s)
 {
     if(*s)
     {
-        reverse(s+1);
+        reverse_char(s+1);
         printf("%c",*s);
     }
 }
 
-
 int main()
 {
-    char *a="hello";
-    reverse(a);
+   int a=9876;
+   char s[]="abcd";
+   reverse(a);
+   printf("\n");
+   reverse_char(s);
     return 0;
 }
