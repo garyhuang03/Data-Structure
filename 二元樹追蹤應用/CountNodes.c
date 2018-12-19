82f058b73afa8d819e39cbf40426e8ceac5f3161
ttiverson3 <ttiverson3@yahoo.com.tw>
2018-12-20 02:33:04 +0800

    Update CountNodes.c

--- 二元樹追蹤應用/CountNodes.c
+++ 二元樹追蹤應用/CountNodes.c
@@ -17,15 +17,6 @@ int count(struct node *s)
     }
 }
 
-void preorder(struct node *T)
-{
-    if(T!=NULL)
-    {
-        cout<<T->data<<" ";
-        preorder(T->left);
-        preorder(T->right);
-    }
-}
 
 int main()
 {
