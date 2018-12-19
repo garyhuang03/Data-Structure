44b50672b70c56bcb39c66dada82bb7edeadf8d2
Gary Huang <45207364+ttiverson3@users.noreply.github.com>
2018-12-19 20:48:05 +0800

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 //107交大
@@ -8,7 +9,7 @@ struct node
 } n1,n2,n3,n4,n5,*f;
 
 //比大小
-void foo1(struct node *p)
+void compare(struct node *p)
 {
     for(;p!=NULL && p->next!=NULL;p=p->next)
     {
@@ -22,56 +23,7 @@ void foo1(struct node *p)
 }
 
 //反轉串列
-void foo2(struct node *p)
-{
-    struct node *q,*r;
-    if(p==NULL) return;
-    q=p->next; p->next=NULL;
-    for(;q!=NULL;p=q,q=r)
-    {
-        r=q->next;
-        q->next=p;
-    }
-    f=p;
-}
-
-//串列相加
-int bar(struct node *p)
-{
-    int flag,sum;
-    for(flag=0,sum=0;p!=NULL;p=p->next,flag++)
-    //表示flag為奇數
-    if(flag&1==1) sum+=p->data;
-    return sum;
-}
-
-//印出串列
-void bar2(struct node *p)
-{
-    while(p!=NULL)
-    {
-        cout<<p->data<<" ";
-        p=p->next;
-    }
-}
-
-int main()
-{
-    f=&n1;
-   n1.data=6; n2.data=3; n3.data=8; n4.data=10; n5.data=1;
-   n1.next=&n2; n2.next=&n3; n3.next=&n4; n4.next=&n5; n5.next=NULL;
-   foo1(f);
-   bar2(f);//印出:3 6 8 1 10 
-   cout<<"\n";
-   foo2(f);
-   bar2(f);//印出:10 1 8 6 3 
-   cout<<"\n";
-   cout<<bar(f);//印出:7
-   return 0;
-}
-
-/*反轉串列另一寫法
-void foo2(struct node *s)
+void invert(struct node *s)
 {
     
     if(s!=NULL)
@@ -86,6 +38,43 @@ void foo2(struct node *s)
         }
          f=q;
     }
+}
+//串列相加
+int sum(struct node *p)
+{
+    int sum;
+    for(sum=0;p!=NULL;p=p->next)
+    sum+=p->data;
+    return sum;
+}
 
-}*/
+//印出串列
+void print(struct node *p)
+{
+    while(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+int main()
+{  //6 3 8 10 1
+    f=&n1;
+   n1.data=6; n2.data=3; n3.data=8; n4.data=10; n5.data=1;
+   n1.next=&n2; n2.next=&n3; n3.next=&n4; n4.next=&n5; n5.next=NULL;
+   
+   compare(f);
+   print(f);//印出:3 6 8 1 10 
+   cout<<"\n";
+   
+   invert(f);
+   print(f);//印出:10 1 8 6 3 
+   cout<<"\n";
+   
+   cout<<sum(f);//印出:28
+   return 0;
+}
+
+
 
